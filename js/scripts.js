99fd766bc90b6f39400c733c257a6ad08a52e77d
eddieduro <student@epicodus.com>
2016-01-12 14:17:51 -0800

    buttons show hexcode for background color

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,16 @@
 $("document").ready(function() {
   $(".btn-blue").click(function() {
     $("body").toggleClass("blue-bg");
+    $(".btn-orange, .btn-green, .blueHidden").toggle();
   });
 
   $(".btn-green").click(function() {
     $("body").toggleClass("green-bg");
+    $(".btn-orange, .btn-blue, .greenHidden").toggle();
   });
 
   $(".btn-orange").click(function() {
     $("body").toggleClass("orange-bg");
+    $(".btn-green, .btn-blue, .orangeHidden").toggle();
   });
 });
