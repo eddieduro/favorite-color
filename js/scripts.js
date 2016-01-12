3bc637899022bdc7fddbadbbfbd5481acfe46463
eddieduro <student@epicodus.com>
2016-01-12 13:54:53 -0800

    buttons toggle background color

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$("document").ready(function() {
+  $(".btn-blue").click(function() {
+    $("body").toggleClass("blue-bg");
+  });
+
+  $(".btn-green").click(function() {
+    $("body").toggleClass("green-bg");
+  });
+
+  $(".btn-orange").click(function() {
+    $("body").toggleClass("orange-bg");
+  });
+});
